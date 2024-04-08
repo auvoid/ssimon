235c50fcd9a5b28b4230085b6e5188005fc831ef
Coodos <info@coodos.co>
2024-04-08 23:43:41 +0530

    feat: multiple adapters

--- src/NetworkAdapter/index.types.ts
+++ src/NetworkAdapter/index.types.ts
@@ -14,6 +14,7 @@ export type NetworkAdapterOptions = {
 export type CreateDidProps<T extends StorageSpec<Record<string, any>, any>> = {
   seed?: string;
   alias: string;
+  method: string;
   store: T;
 };
 
@@ -21,9 +22,13 @@ export declare class NetworkAdapter {
   public static build<T extends IdentityAccount>(
     options: NetworkAdapterOptions
   ): Promise<NetworkAdapter>;
+
+  public getMethodIdentifier(): string;
+
   public createDid<T extends StorageSpec<Record<string, any>, any>>(
     props: CreateDidProps<T>
   ): Promise<DidCreationResult>;
+
   public deserializeDid<T extends StorageSpec<Record<string, any>, any>>(
     conf: IdentityConfig,
     store: T
